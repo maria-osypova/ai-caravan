2bb24ff8b2e79775616b89017df5eb1c475cd02c
Maria Osypova <mosipova1998@gmail.com>
2025-01-30 14:34:12 +0100

    add UI for Login

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import dbConnect from "@/db/connect";
+import User from "@/db/models/User";
 
 export const authOptions = {
   providers: [
@@ -8,6 +10,13 @@ export const authOptions = {
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
+  callbacks: {
+    async signIn({ user }) {
+      await dbConnect();
+      const existingUser = await User.findOne({ email: user.email });
+      return existingUser ? true : "/guest/onboarding";
+    },
+  },
 };
 
 export default NextAuth(authOptions);
