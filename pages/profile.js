bc85cc90842209fa10f8fbb4ff4ec6df41c74136
Maria Osypova <mosipova1998@gmail.com>
2025-02-02 01:03:34 +0100

    update profile page and edit mode

--- pages/profile.js
+++ pages/profile.js
@@ -1,18 +1,29 @@
 import useSWR, { mutate } from "swr";
 import { useState } from "react";
 import Image from "next/image";
-import { ProfileContainer } from "@/components/ProfileContainer";
-import { StyledButton } from "@/components/StyledButton";
 import EditProfileForm from "@/components/EditProfileForm";
-import { Button } from "@mui/material";
+import { Button, Container, Grid, Typography, Box } from "@mui/material";
 
 export default function UserProfile() {
   const { data, error, isLoading } = useSWR("/api/user");
   const [isEditing, setIsEditing] = useState(false);
   if (error) return <p>Failed to load user data</p>;
   if (isLoading) return <p>Loading...</p>;
-  const { _id, firstName, lastName, email, linkedin, role, expertise, photo } =
-    data;
+  const {
+    _id,
+    firstName,
+    lastName,
+    email,
+    linkedin,
+    role,
+    expertise,
+    photo,
+    level,
+    company,
+    city,
+    goal,
+    unemployed,
+  } = data;
 
   function handleEditClick() {
     setIsEditing(true);
@@ -47,22 +58,90 @@ export default function UserProfile() {
           onSubmit={handleFormSubmit}
         />
       ) : (
-        <ProfileContainer>
-          <Image
-            src={photo}
-            alt={`${firstName} ${lastName}`}
-            width={100}
-            height={100}
-            style={{ borderRadius: "50%" }}
-          />
-          <p>{firstName}</p>
-          <p>{lastName}</p>
-          <p>{email}</p>
-          <p>{linkedin}</p>
-          <p>{role}</p>
-          <p>{expertise}</p>
-          <Button onClick={handleEditClick}>Edit</Button>
-        </ProfileContainer>
+        <Container maxWidth="md">
+          <Box sx={{ my: 4 }}>
+            <Typography variant="h4" gutterBottom>
+              Profile
+            </Typography>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <Image
+                  src={photo || "/images/default-avatar.jpg"}
+                  alt="Profile Photo"
+                  width={95}
+                  height={92}
+                  style={{
+                    borderRadius: "24px",
+                  }}
+                />
+              </Grid>
+              <Grid item xs={6}>
+                <Typography variant="body1">{firstName}</Typography>
+              </Grid>
+              <Grid item xs={6}>
+                <Typography variant="body1">{lastName}</Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography variant="body1">{email}</Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography variant="h6">Experience</Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography variant="body1">
+                  <strong>Role:</strong> {role}
+                </Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography variant="body1">
+                  <strong>Level:</strong> {level}
+                </Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography variant="body1">
+                  <strong>Expertise:</strong> {expertise}
+                </Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography variant="body1">
+                  <strong>Company Name:</strong> {company}
+                </Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography variant="h6">Additional Information</Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography variant="body1">
+                  <strong>City:</strong> {city}
+                </Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography variant="body1">
+                  <strong>LinkedIn:</strong> {linkedin}
+                </Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography variant="body1">
+                  <strong>Primary Goal:</strong> {goal}
+                </Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Button
+                  variant="contained"
+                  style={{
+                    backgroundColor: "#2525A7",
+                    color: "white",
+                    borderRadius: "32px",
+                    marginBottom: "74px",
+                  }}
+                  onClick={handleEditClick}
+                >
+                  EDIT
+                </Button>
+              </Grid>
+            </Grid>
+          </Box>
+        </Container>
       )}
     </>
   );
